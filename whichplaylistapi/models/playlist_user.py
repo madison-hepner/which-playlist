2c079c3a7b3c7b0a7afd9d17066c0741b5326399
madison-hepner <hepnermadison@icloud.com>
2022-08-25 16:56:44 -0500

    create and update

---
+++ whichplaylistapi/models/playlist_user.py
@@ -0,0 +1,8 @@
+# from django.db import models
+# from django.contrib.auth.models import User
+
+
+# class PlayListUser(models.Model):
+
+#     playlistuser = models.OneToOneField(User, on_delete=models.CASCADE)
+#     bio = models.CharField(max_length=50)
--- whichplaylistapi/views/playlist_item.py
+++ whichplaylistapi/views/playlist_item.py
@@ -30,9 +30,39 @@ class PlayListView(ViewSet):
         serializer = PlayListSerializer(playlist, many=True)
         return Response(serializer.data)
 
+    def create(self, request):
+        playlist = PlayListItem.objects.create(
+            playlist_name=request.data["playlist_name"],
+            description=request.data["description"],
+            playlist_vibes=request.data["playlist_vibes"],
+            length=request.data["length"]
+        )
+        serializer = PlayListSerializer(playlist)
+        return Response(serializer.data)
+
+    def update(self, request, pk):
+        playlist = PlayListItem.objects.get(pk=pk)
+        playlist.playlist_name = request.data["playlist_name"]
+        playlist.description = request.data["description"]
+        playlist.playlist_vibes = request.data["playlist_vibes"]
+        playlist.length = request.data["length"]
+
+        # game_type = GameType.objects.get(pk=request.data["game_type"])
+        # game.game_type = game_type
+        playlist.save()
+
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
 
 class PlayListSerializer(serializers.ModelSerializer):
     class Meta:
         model = PlayListItem
         fields = ('id', 'playlist_name', 'description',
                   'playlist_vibes', 'length')
+
+
+class CreatePlayListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PlayListItem
+        fields = ['id',  'playlist_name', 'description',
+                  'playlist_vibes', 'length']
